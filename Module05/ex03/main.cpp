e74529001fc8217ab16dee06b853d43412bc8e2f
Pedrocasss <pedroacnesisma13@gmail.com>
2024-03-13 17:44:07 +0000

    improving main on ex03 (now no leaks)

--- Module05/ex03/main.cpp
+++ Module05/ex03/main.cpp
@@ -4,36 +4,41 @@
 
 int main()
 {
+  AForm *b = NULL;
+  AForm *c = NULL;
+
   try
   {
     srand(time(0));
-    Bureaucrat a("Emperor", 1);
+    Bureaucrat a("Emperor", 45);
     Intern z;
-
-    AForm *b = z.makeForm("RobotomyRequestForm", "Bender");
-    AForm *c = z.makeForm("ShrubberyCreationForm", "Christmas");
-
+    b = z.makeForm("RobotomyRequestForm", "Bender");
+    c = z.makeForm("ShrubberyCreationForm", "Christmas");
     std::cout << a << std::endl << *b << std::endl << *c << std::endl;
-
     b->beSigned(a);
     a.signForm(*c);
-
     std::cout << *b << std::endl << *c << std::endl;
 
     for (int i = 0; i < 10; i++)
       b->execute(a);
 
     a.executeForm(*c);
-
-    delete b;
-    delete c;
-  } catch (Bureaucrat::GradeTooHighException &exception) {
-    std::cerr << "Caught GradeTooHighException: " << exception.what()
-              << std::endl;
-  } catch (Bureaucrat::GradeTooLowException &exception) {
-    std::cerr << "Caught GradeTooLowException: " << exception.what()
-              << std::endl;
-  } catch (std::exception &exception) {
+  }
+  catch (Bureaucrat::GradeTooHighException &exception)
+  {
+    std::cerr << "Caught GradeTooHighException: " << exception.what() << std::endl;
+  }
+  catch (Bureaucrat::GradeTooLowException &exception)
+  {
+    std::cerr << "Caught GradeTooLowException: " << exception.what() << std::endl;
+  }
+  catch (std::exception &exception)
+  {
     std::cerr << "Caught exception: " << exception.what() << std::endl;
   }
+
+  delete b;
+  delete c;
+
+  return 0;
 }
